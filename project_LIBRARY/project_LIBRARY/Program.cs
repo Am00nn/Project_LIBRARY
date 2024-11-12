324d819556c250eb3b8ace9f3cc2a612d0b1846b
Amani <amanialshmali7@gmail.com>
2024-11-12 15:56:39 +0400

    pp

---
+++ project_LIBRARY/project_LIBRARY/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_LIBRARY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
